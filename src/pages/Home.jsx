a7d0923b7c24f663831e6aa24ab9988d68086699
Dhanushkumar04 <dkdhanush1970@gmail.com>
2026-05-18 14:19:03 +0530

    Update HERO_TOUR_URL to local for dev preview

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const heroTitles = [
   "Let Customers Explore Before They Visit"
 ];
 
-const HERO_TOUR_URL = "https://mania3d-assets.web.app/presidency-school-east/index.html";
+const HERO_TOUR_URL = "/tours/presidency-school-east/index.html";
 
 const Home = () => {
   const [currentTitle, setCurrentTitle] = useState(0);
